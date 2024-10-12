2ae85807e5f54e205a0e5927a8cb52c3b69923a0
Maria Emilia Naves <meminaves@gmail.com>
2024-10-12 12:34:30 -0300

    Se añade documentación del ejercicio 2 de la guia 2

--- firmware/projects/Guia 2 - Ejercicio 2/main/Guia 2 - Ejercicio 2.c
+++ firmware/projects/Guia 2 - Ejercicio 2/main/Guia 2 - Ejercicio 2.c
@@ -1,16 +1,24 @@
-/*! @mainpage Guia 2 Ejercicio 1
+/*! @mainpage Guia 2 Ejercicio 2
  *
  * @section Descripción
  *
  * Medidor de distancia por ultrasonido
+ * 
+ * En este ejercicio se implementan las mismas funcionalidades que el ejercicio anterior
+ * Sin embargo, se utilizan interrupciones para el control de teclas y timers.
  *
  *
  * @section hardConn Hardware Connection
  *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
- *
+ * | Peripheral    | ESP32        |
+ * |---------------|--------------|
+ * |  HC-SR04 Trig | GPIO_3       |
+ * |  HC-SR04 Echo | GPIO_2       |
+ * |  LED 1        | GPIO_20      |
+ * |  LED 2        | GPIO_21      |
+ * |  LED 3        | GPIO_22      |
+ * |  Switch 1     | GPIO_4       |
+ * |  Switch 2     | GPIO_15      |
  *
  * @section changelog Changelog
  *
@@ -35,40 +43,80 @@
 #include <gpio_mcu.h>
 
 /*==================[macros and definitions]=================================*/
+
+/**
+ * @brief Almacena el valor de la distancia medida
+ */
 int MEDIDA;
+
+/**
+ * @brief Estado de encendido del sistema de medición
+ */
 bool ON = true;
+
+/**
+ * @brief Estado de retención de la última medida mostrada en pantalla
+ */
 bool HOLD = false;
 
-#define CONFIG_BLINK_PERIOD_LED 500
-#define RETARDO_MOSTRAR 500
-#define RETARDO_MEDIR 1000
-#define RETARDO_TECLAS 300
-#define CONFIG_BLINK_PERIOD_LED_1_US 1000000
+#define PERIODO 1000000 /**< Periodo de los timers */
 
 /*==================[internal data definition]===============================*/
+
+/**
+ * @brief Task handle de la tarea que muestra la medida en la pantalla
+ */
 TaskHandle_t taskMostrarMedida_task_handle = NULL;
+
+
+/**
+ * @brief Task handle de la tarea que toma las medidas de distancia
+ */
 TaskHandle_t taskTomarMedida_task_handle = NULL;
 
 /*==================[internal functions declaration]=========================*/
+
 /**
- * @brief Función invocada en la interrupción del timer A
+ * @fn void FuncTimerTeclas(void *param)
+ * @brief Función invocada por la interrupción del timer A para notificar a la tarea de mostrar la medida.
+ * @param param Parámetro de la tarea (no utilizado).
  */
 void FuncTimerTeclas(void* param){
     vTaskNotifyGiveFromISR(taskMostrarMedida_task_handle, pdFALSE);  	
 }
 
+/**
+ * @fn void FuncTimerTomarMedida(void* param)
+ * @brief Función invocada por la interrupción del timer B para notificar a la tarea de toma de medida.
+ * @param param Parámetro de la tarea (no utilizado).
+ */
 void FuncTimerTomarMedida(void* param){
     vTaskNotifyGiveFromISR(taskTomarMedida_task_handle, pdFALSE);    	
 }
 
+/**
+ * @brief Función para manejar la lógica de la tecla 1. Cambia el estado de la variable ON.
+ */
 void tecla1(){
 	ON = !ON;
 }
 
+/**
+ * @brief Función para manejar la lógica de la tecla 2. Cambia el estado de la variable HOLD.
+ */
 void tecla2(){
 	HOLD = !HOLD;
 }
 
+/**
+ * @fn static void manejarLeds()
+ * @brief Controla el encendido de los LEDs según la distancia medida
+ *
+ * Si la distancia es menor a 10 cm, todos los LEDs están apagados.
+ * Si la distancia está entre 10 y 20 cm, se enciende el LED_1.
+ * Si la distancia está entre 20 y 30 cm, se encienden los LEDs 1 y 2.
+ * Si la distancia es mayor a 30 cm, se encienden todos los LEDs.
+ */
 static void manejarLeds()
 {
 		LedsOffAll();
@@ -98,6 +146,16 @@ static void manejarLeds()
 			LedOn(LED_3);
 		}
 }
+
+/**
+ * @fn static void taskMostrarMedida(void *pvParameter)
+ * @brief Tarea que muestra la medida en la pantalla y maneja los LEDs
+ *
+ * Si el sistema está encendido (ON), se muestra la medida en la pantalla y se controlan los LEDs
+ * según la distancia medida. Si el sistema está en modo HOLD, la medida no se actualiza.
+ *
+ * @param pvParameter Parámetro de la tarea (no utilizado)
+ */
 static void taskMostrarMedida(void *pvParameter)
 {
 	while (true)
@@ -123,6 +181,14 @@ static void taskMostrarMedida(void *pvParameter)
 	}
 }
 
+/**
+ * @fn taskTomarMedida(void *pvParameter)
+ * @brief Tarea que toma las medidas de distancia utilizando el sensor ultrasónico
+ *
+ * La medida se almacena en la variable global MEDIDA.
+ *
+ * @param pvParameter Parámetro de la tarea (no utilizado)
+ */
 static void taskTomarMedida(void *pvParameter)
 {
 	while(true)
@@ -135,6 +201,12 @@ static void taskTomarMedida(void *pvParameter)
 
 /*==================[external functions definition]==========================*/
 
+/**
+ * @brief Función principal del programa.
+ *
+ * Inicializa los periféricos (LEDs, LCD, sensor ultrasónico, teclas), configura los timers
+ * para manejar las teclas y la toma de medidas, y crea las tareas de FreeRTOS.
+ */
 void app_main(void)
 {
 	LedsInit();
@@ -146,7 +218,7 @@ void app_main(void)
 	 /* Inicialización de timers */
     timer_config_t timer_teclas = {
         .timer = TIMER_A,
-        .period = CONFIG_BLINK_PERIOD_LED_1_US,
+        .period = PERIODO,
         .func_p = FuncTimerTeclas,
         .param_p = NULL
     };
@@ -154,19 +226,22 @@ void app_main(void)
 
     timer_config_t timer_tomar_medida = {
         .timer = TIMER_B,
-        .period = CONFIG_BLINK_PERIOD_LED_1_US,
+        .period = PERIODO,
         .func_p = FuncTimerTomarMedida,
         .param_p = NULL
     };
 
     TimerInit(&timer_tomar_medida);
 
+ 	/* Creación de tareas */
 	xTaskCreate(&taskTomarMedida, "Tomar Medida", 2048, NULL, 5, &taskTomarMedida_task_handle); 
 	xTaskCreate(&taskMostrarMedida, "Manejar Leds", 2048, NULL, 5, &taskMostrarMedida_task_handle);
 
+	/* Activación de interrupciones de teclas */
 	SwitchActivInt(SWITCH_1,*tecla1,NULL); 
 	SwitchActivInt(SWITCH_2,*tecla2,NULL);
 
+	/* Inicio de timers */
   	TimerStart(timer_teclas.timer);
 	TimerStart(timer_tomar_medida.timer);
 }
