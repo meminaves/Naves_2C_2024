c747820e9f27f4a3e80a5d3e61066dc0ad8c14f9
Maria Emilia Naves <meminaves@gmail.com>
2024-11-04 18:43:23 -0300

    Se añade documentación

--- firmware/projects/examenEP/main/examenEP.c
+++ firmware/projects/examenEP/main/examenEP.c
@@ -2,24 +2,35 @@
  *
  * @section genDesc General Description
  *
- * This section describes how the program works.
+ * Aplicación que implementa una alerta para ciclistas
  *
- * <a href="https://drive.google.com/...">Operation Example</a>
  *
  * @section hardConn Hardware Connection
  *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
- *
- *
+ * | Peripheral    | ESP32        |
+ * |---------------|--------------|
+ * |  HC-SR04 Trig | GPIO_7       |
+ * |  HC-SR04 Echo | GPIO_8       |
+ * |  LED 1        | GPIO_20      |
+ * |  LED 2        | GPIO_21      |
+ * |  LED 3        | GPIO_22      |
+ * |  Buzzer       | GPIO_6       |
+ * |  Switch 2     | GPIO_15      |
+ * |  UART         | UART_CONNECTION    |
+ * | 	ADC_CH0  	| 	GPIO_0		|
+ * | 	ADC_CH1  	| 	GPIO_1		|
+ * | 	ADC_CH2  	| 	GPIO_2		|
+ * | 	ADC_CH2  	| 	GPIO_2		|
+ * | 	TX      	| 	GPIO_18		|
+ * | 	RX      	| 	GPIO_19	    |
+
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
  * | 12/09/2023 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Maria Emilia Naves
  *
  */
 
@@ -36,41 +47,73 @@
 #include "uart_mcu.h"
 
 /*==================[macros and definitions]=================================*/
+/*! @brief Variable que almacena la distancia medida por el sensor de ultrasonido (en cm) */
 int DISTANCIA;
 
-/*! @brief Variable que almacena el valor del voltaje leído */
+/*! @brief Variable que almacena el valor del voltaje leído del eje X del acelerómetro */
 float VOLTAJE_X = 0;
 
-/*! @brief Variable que almacena el valor del voltaje leído */
+/*! @brief Variable que almacena el valor del voltaje leído del eje Y del acelerómetro */
 float VOLTAJE_Y = 0;
 
-/*! @brief Variable que almacena el valor del voltaje leído */
+/*! @brief Variable que almacena el valor del voltaje leído del eje Z del acelerómetro */
 float VOLTAJE_Z = 0;
 
+/*! @brief Variable que almacena el valor de aceleración del eje X */
 float AC_X;
 
+/*! @brief Variable que almacena el valor de aceleración del eje Y */
 float AC_Y;
 
+/*! @brief Variable que almacena el valor de aceleración del eje Z */
 float AC_Z;
 
+/*! @brief Variable que almacena la sumatoria escalar de aceleración*/
 float AC_total;
 
+/*! @brief Variable que determina que el dispositivo está encendido. Podria en un futuro implementarse un boton de apagado/encendido */
 bool ON = true;
 
+/**
+ * @def SENSIBLIDIDAD_AC
+ * @brief Sensibilidad del acelerómetro
+ */
 #define SENSIBILIDAD_AC 0.3f
 
+/**
+ * @def GPIO_BUZZER
+ * @brief GPIO destinado al buzzer, en este caso el GPIO_6
+ */
 #define GPIO_BUZZER GPIO_6
 
-#define DELAY_PRECAUCION 1000 //1 s
+/**
+ * @def DELAY_PRECAUCION
+ * @brief Delay del buzzer cuando se encuentra en estado de precaución
+ */
+#define DELAY_PRECAUCION 500 //1 s -> durante 500 ms está encendido y durante los otros 500 ms está apagado
 
-#define DELAY_PELIGRO 500 //0,5 s
+/**
+ * @def DELAY_PELIGRO
+ * @brief Delay del buzzer cuando se encuentra en estado de peligro
+ */
+#define DELAY_PELIGRO 250 //0,5 s -> durante 250 ms está encendido y durante los otros 250 ms está apagado
 
+/**
+ * @def AC_CAIDA
+ * @brief Aceleración para la cual se considera que existe una caída
+ */
 #define AC_CAIDA 4.0f
 
-#define CONFIG_BLINK_PERIOD_TIMER_B 500000 /**< Periodo del timer B */
+/*! @brief Período del temporizador para la lectura de la distancia */
+#define CONFIG_BLINK_PERIOD_TIMER_B 500000 
+
+/*! @brief Periodo del temporizador para la medida de aceleración*/
 #define CONFIG_BLINK_PERIOD_TIMER_A 10000 //10000 us => frecuencia de 100 Hz
+
+/*! @brief Período del temporizador para el manejo del sistema */
 #define CONFIG_BLINK_PERIOD_TIMER_C 8000 
 
+/*! @brief typedef que define los posibles estados de distancia del sistema*/
 typedef enum estados_de_distancia
 {
 	DISTANCIA_ACEPTABLE,
@@ -79,14 +122,17 @@ typedef enum estados_de_distancia
 
 } estado_distancia;
 
+/*! @brief variable de tipo estado_distancia que almacena el valor actual del sistema */
 estado_distancia ESTADO_DISTANCIA_ACTUAL;
 
 /*==================[internal data definition]===============================*/
-
+/*! @brief Task handle para la tarea Tomar Distancia */
 TaskHandle_t taskTomarDistancia_task_handle = NULL;
 
+/*! @brief Task handle para la tarea Medir Aceleración*/
 TaskHandle_t  taskMedirAceleracion_task_handle = NULL;
 
+/*! @brief Task handle para la tarea de manejo del sistema */
 TaskHandle_t taskManejarSistema_task_handle = NULL;
 
 /*==================[internal functions declaration]=========================*/
@@ -95,27 +141,40 @@ TaskHandle_t taskManejarSistema_task_handle = NULL;
  * @brief Función invocada por la interrupción del timer B para notificar a la tarea de toma de distancia.
  * @param param Parámetro de la tarea (no utilizado).
  */
-
 void FuncTimerTomarDistancia(void* param)
 {
     vTaskNotifyGiveFromISR(taskTomarDistancia_task_handle, pdFALSE);    	
 }
 
+ /** @fn void FuncTimerMedirAceleracion(void* param)
+ * @brief Función invocada por la interrupción del timer A para notificar a la tarea de medición de aceleración.
+ * @param param Parámetro de la tarea (no utilizado).
+ */
 void FuncTimerMedirAceleracion(void* param)
 {
     vTaskNotifyGiveFromISR(taskMedirAceleracion_task_handle, pdFALSE);    	
 }
 
+ /** @fn void FuncTimerManejarSistema(void* param)
+ * @brief Función invocada por la interrupción del timer C para notificar a la tarea de manejo del sistema
+ * @param param Parámetro de la tarea (no utilizado).
+ */
 void FuncTimerManejarSistema(void* param)
 {
     vTaskNotifyGiveFromISR(taskManejarSistema_task_handle, pdFALSE);    	
 }
 
+ /** @fn void inicializarBuzzer()
+ * @brief Función que inicializa el buzzer
+ */
 void inicializarBuzzer()
 {
 	void GPIOInit(GPIO_6, GPIO_OUTPUT);
 }
 
+ /** @fn void detectarEstadoDeDistancia()
+ * @brief función que detecta el estado del sistema 
+ */
 void detectarEstadoDeDistancia()
 {
     if (DISTANCIA >= 5)
@@ -131,6 +190,10 @@ void detectarEstadoDeDistancia()
 		ESTADO_DISTANCIA_ACTUAL = DISTANCIA_PELIGRO;
 	}
 }
+
+ /** @fn void inicializarAcelerometro()
+ * @brief función que inicializa el acelerometro
+ */
 void inicializarAcelerometro()
 {
 	analog_input_config_t presion_config1 = {
@@ -151,8 +214,9 @@ void inicializarAcelerometro()
 	AnalogInputInit(&presion_config3);
 }
 
-
-
+ /** @fn void encenderBuzzerPrecaucion()
+ * @brief función que enciende el buzzer con una frecuencia de un segundo
+ */
 void encenderBuzzerPrecaucion()
 {
 	GPIOToggle(GPIO_BUZZER);
@@ -160,6 +224,9 @@ void encenderBuzzerPrecaucion()
 	vTaskDelay(DELAY_PRECAUCION / portTICK_PERIOD_MS);
 }
 
+ /** @fn void encenderBuzzerPeligro()
+ * @brief función que enciende el buzzer con una frecuencia de medio segundo
+ */
 void encenderBuzzerPeligro()
 {
 	GPIOToggle(GPIO_BUZZER);
@@ -167,11 +234,17 @@ void encenderBuzzerPeligro()
 	vTaskDelay(DELAY_PELIGRO / portTICK_PERIOD_MS);
 }
 
+ /** @fn void apagarBuzzer()
+ * @brief función que apaga el buzzer
+ */
 void apagarBuzzer()
 {
 	GPIOOff(GPIO_BUZZER);
 }
 
+ /** @fn void manejarPerifericos()
+ * @brief función que maneja los leds y el buzzer en funcion de la distancia medida (estado)
+ */
 void manejarPerifericos()
 {
 		LedsOffAll();
@@ -199,6 +272,9 @@ void manejarPerifericos()
 		}
 }
 
+ /** @fn void enviarDatosApp()
+ * @brief función que envia los datos a la aplicación bluetooth por el puerto de uart connection
+ */
 void enviarDatosApp()
 {
 	if (ESTADO_DISTANCIA_ACTUAL == DISTANCIA_PRECAUCION)
@@ -215,7 +291,14 @@ void enviarDatosApp()
 		UartSendString(UART_CONNECTOR, "Caida detectada\n");
 	}
 }
-
+/**
+ * @fn taskTomarDistancia(void *pvParameter)
+ * @brief Tarea que toma las medidas de distancia utilizando el sensor ultrasónico
+ *
+ * La medida se almacena en la variable global Distancia.
+ *
+ * @param pvParameter Parámetro de la tarea (no utilizado)
+ */
 static void taskTomarDistancia(void *pvParameter)
 {
 	while(true)
@@ -224,6 +307,12 @@ static void taskTomarDistancia(void *pvParameter)
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	}
 }
+
+/**
+ * @fn taskManejarSistema()
+ * @brief Tarea que maneja el sistema completo, tanto los perifericos como el envio de datos 
+ *
+ */
 static void taskManejarSistema(){
 
     while (true)
@@ -245,6 +334,10 @@ static void taskManejarSistema(){
 	}
 }
 
+/**
+ * @fn taskTomarDistancia(void *pvParameter)
+ * @brief Tarea que mide la aceleración en todos los ejes y saca la suma escalar
+ */
 static void taskMedirAceleracion()
 {
 	while (true)
