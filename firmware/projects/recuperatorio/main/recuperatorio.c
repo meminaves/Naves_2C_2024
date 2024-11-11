17452041248ff137731d9085f320db83d22d9f29
Maria Emilia Naves <meminaves@gmail.com>
2024-11-11 16:58:46 -0300

    Se inicializan los leds y se implementa el control de los mismos de acuerdo a la velocidad medida

--- firmware/projects/recuperatorio/main/recuperatorio.c
+++ firmware/projects/recuperatorio/main/recuperatorio.c
@@ -69,6 +69,36 @@ void FuncTimerMedirVelocidad(void* param)
 {
     vTaskNotifyGiveFromISR(taskMedirVelocidad_task_handle, pdFALSE);    	
 }
+
+/**
+ * @fn void controlarLeds()
+ * @brief Controla el encendido de los LEDs según la velocidad medida
+ *
+ * Si la velocidad es mayor a 8 m/s, se enciende el LED3 ()
+ * Si la velocidad se encuentra entre 0 m/s y 8 m/s, se enciende el LED2
+ * Si el vehículo está detenido, se enciende el LED1
+ */
+void controlarLeds()
+{
+		if (VELOCIDAD > 8)
+		{
+			LedOn(LED_3);
+			LedOff(LED_2);
+			LedOff(LED_1);
+		}
+		else if(VELOCIDAD > 0 && VELOCIDAD <= 8)
+		{
+			LedOff(LED_3);
+			LedOn(LED_2);
+			LedOff(LED_1);
+		}
+		else if(VELOCIDAD == 0) //Asumo que la velocidad no puede ser negativa por que el vehículo no retrocede
+		{
+			LedOff(LED_3);
+			LedOff(LED_2);
+			LedOn(LED_1);
+		}
+}
 /**
  * @fn taskMedirVelocidad(void *pvParameter)
  * @brief Tarea que toma las medidas de distancia utilizando el sensor ultrasónico y además utiliza estas mismas para calcular la velocidad del vehículo
@@ -77,17 +107,19 @@ void FuncTimerMedirVelocidad(void* param)
  *
  * @param pvParameter Parámetro de la tarea (no utilizado)
  */
+
 static void taskMedirVelocidad(void *pvParameter)
 {
 	while(true)
 	{
 		DISTANCIA_ANTERIOR = DISTANCIA;
 		DISTANCIA = HcSr04ReadDistanceInCentimeters();
+		LedsOffAll();
 
 		if (DISTANCIA < 1000) //Si el vehículo se encuentra a menos de 1000 cm (10 m)...
 		{
 			VELOCIDAD = (DISTANCIA_ANTERIOR - DISTANCIA)/(0,1*100); 		//delta de desplazamiento sobre el tiempo que 																	
-																			//transcurrió entre ambos puntos (es decir el tiempo de muestreo)
+			controlarLeds();																		//transcurrió entre ambos puntos (es decir el tiempo de muestreo)
 		}																	//Además se divide por 100 para convertir de cm a m
 			
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
@@ -99,7 +131,7 @@ void app_main(void)
 {
 	/*Inicialización de periféricos*/
 	HcSr04Init(GPIO_3, GPIO_2); 
-
+	LedsInit();
 
 	/*Timers */
 	timer_config_t timer_tomar_distancia = {
