f11af60b0adf919f54108895badb8dc559d4e80a
Maria Emilia Naves <meminaves@gmail.com>
2024-11-11 16:20:46 -0300

    Se crea proyecto para el recuperatorio

---
+++ firmware/projects/recuperatorio/main/recuperatorio.c
@@ -0,0 +1,39 @@
+/*! @mainpage Recuperatorio EP
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 11/11/2024 | Document creation		                         |
+ *
+ * @author Maria Emilia Naves
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
